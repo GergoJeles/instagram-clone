979dde96bded5e1bd9f8e68d7d987436cde5f6e3
Gergö <gergojeles5@gmail.com>
2024-01-26 12:48:43 +0100

    searchbar, icons were added

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -12,15 +12,52 @@ import AccountOutline from "vue-material-design-icons/AccountOutline.vue";
 import ChevronLeft from "vue-material-design-icons/ChevronLeft.vue";
 import AccountPlusOutline from "vue-material-design-icons/AccountPlusOutline.vue";
 
-import MenuItem from "@/Components/MenuItem.vue";
-import CreatePostOverlay from "@/Components/CreatePostOverlay.vue";
-
 let showCreatePost = ref(false);
 </script>
 
 <template>
-    <div id="MainLayout" class="bg-red-600">
-        <div>MainLayout</div>
-        <slot />
+    <div id="MainLayout" class="w-full h-screen">
+        <div
+            v-if="$page.url === '/'"
+            id="TopNavHome"
+            class="fixed z-30 md:hidden block w-full bg-white h-[61px] border-b border-b-gray-300"
+        >
+            <div class="flex items-center justify-between h-full">
+                <Link href="/">
+                    <img
+                        class="w-[105px] ml-6 cursor-pointer"
+                        src="/insta-logo.png"
+                    />
+                </Link>
+                <div class="flex items-center w-[50%]">
+                    <div
+                        class="flex items-center w-full bg-gray-100 rounded-lg"
+                    >
+                        <Magnify class="pl-4" fillColor="#8E8E8E" :size="27" />
+                        <input
+                            type="text"
+                            placeholder="Search"
+                            class="bg-transparent w-full placeholder-[#8E8E8E] border-0 ring-0 focus:ring-0"
+                        />
+                    </div>
+                    <HeartOutline
+                        class="pl-4 pr-3"
+                        fillColor="#000000"
+                        :size="27"
+                    />
+                </div>
+            </div>
+        </div>
+        <div
+            v-if="$page.url !== '/'"
+            id="TopNavUser"
+            class="md:hidden fixed flex items-center justify-between z-30 w-full bg-white h-[61px] border-b border-b-gray-300"
+        >
+            <Link href="/" class="px-4">
+                <ChevronLeft :size="30" class="cursor-pointer" />
+            </Link>
+            <div class="font-extrabold text-lg">NAME HERE</div>
+            <AccountPlusOutline :size="30" class="cursor-pointer px-4" />
+        </div>
     </div>
 </template>
